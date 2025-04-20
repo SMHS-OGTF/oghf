fb1308fd5cf2d90b0b4e00f03012e83f8d7f99ab
Jean BOURGUIGNON <jeanbourguignon@Jeans-MacBook-Pro.local>
2025-04-19 21:11:26 -0400

    Admin UI polished up, need to add DB operations

--- components/EditableTable.js
+++ components/EditableTable.js
@@ -42,32 +42,30 @@ export default function EditableTable({ items, teamList, fields, headers, editab
     };
 
     return (
-        <div className="overflow-x-auto border border-gray-300 rounded-xl">
-            <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-uiDark text-uiLight">
-                    <tr>
+        <div className="overflow-auto">
+            <table className="table table-fixed text-uiDark text-base">
+                <thead>
+                    <tr className="text-lg font-bold bg-uiDark text-uiBg">
                         {headers.map((header) => (
-                            <th key={header} className="px-4 py-2 text-left capitalize">
+                            <th key={header} className="py-2">
                                 {header}
                             </th>
                         ))}
-                        {editable && <th className="px-4 py-2 max-w-fit">Actions</th>}
+                        {editable && <th className="py-2 w-32 text-center">Actions</th>}
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="cssZebraRow">
                     {items.map((item, idx) => (
-                        <tr key={idx} className="odd:bg-white even:bg-gray-100">
+                        <tr key={idx} className="border-0">
                             {fields.map((field) => (
-                                <td key={field} className="px-4 py-2">
+                                <td key={field} className="py-2">
                                     {editingIndex === idx ? (
-                                        // Render dropdowns for team fields (except "displayName")
                                         teamFieldSelectionList.includes(field) ? (
                                             <select
                                                 value={editedRow[field] || ''}
                                                 onChange={(e) => handleChange(field, e.target.value)}
-                                                className="border rounded px-2 py-1 w-full"
+                                                className="bg-transparent text-uiDark w-full rounded-md outline-[1px] outline-uiDark outline-none appearance-none pr-8 bg-[url('/chevron.svg')] bg-no-repeat bg-right bg-[length:1rem_1rem]"
                                             >
-                                                <option value="">Select Team</option>
                                                 {teamList.map(team => (
                                                     <option key={team.id} value={team.id}>
                                                         {team.displayName}
@@ -75,16 +73,14 @@ export default function EditableTable({ items, teamList, fields, headers, editab
                                                 ))}
                                             </select>
                                         ) : (
-                                            // Render textboxes for non-team fields or "displayName"
                                             <input
                                                 type="text"
-                                                className="border rounded px-2 py-1 w-full"
+                                                className="bg-uiLight text-uiDark w-full rounded-md outline-[1px] outline-uiDark outline-none"
                                                 value={editedRow[field] || ''}
                                                 onChange={(e) => handleChange(field, e.target.value)}
                                             />
                                         )
                                     ) : (
-                                        // Display team names for team fields or raw values for others
                                         teamFieldSelectionList.includes(field) ? (
                                             getTeamName(item[field]) || 'Unknown Team'
                                         ) : (
@@ -94,35 +90,59 @@ export default function EditableTable({ items, teamList, fields, headers, editab
                                 </td>
                             ))}
                             {editable && (
-                                <td className="px-4 py-2 space-x-2 whitespace-nowrap max-w-fit">
+                                <td className="py-2 space-x-2 whitespace-nowrap w-32 text-center">
                                     {editingIndex === idx ? (
                                         <>
-                                            <button
+                                            <svg 
+                                                xmlns="http://www.w3.org/2000/svg" 
+                                                fill="none" 
+                                                viewBox="0 0 24 24" 
+                                                strokeWidth={1.5} 
+                                                stroke="currentColor" 
+                                                className="size-6 text-green-600 inline"
                                                 onClick={saveEdit}
-                                                className="text-green-600 hover:underline"
                                             >
-                                                Save
-                                            </button>
-                                            <button
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                            </svg>
+
+                                            <svg 
+                                                xmlns="http://www.w3.org/2000/svg" 
+                                                fill="none" 
+                                                viewBox="0 0 24 24" 
+                                                strokeWidth={1.5} 
+                                                stroke="currentColor" 
+                                                className="size-6 text-gray-500 inline"
                                                 onClick={cancelEdit}
-                                                className="text-gray-500 hover:underline"
                                             >
-                                                Cancel
-                                            </button>
-                                            <button
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                            </svg>
+
+                                            <svg 
+                                                xmlns="http://www.w3.org/2000/svg" 
+                                                fill="none" 
+                                                viewBox="0 0 24 24" 
+                                                strokeWidth={1.5} 
+                                                stroke="currentColor" 
+                                                className="size-6 text-red-600 inline"
                                                 onClick={deleteRow}
-                                                className="text-red-600 hover:underline"
                                             >
-                                                Delete
-                                            </button>
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+
                                         </>
                                     ) : (
-                                        <button
+                                        <svg 
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            fill="none" 
+                                            viewBox="0 0 24 24" 
+                                            strokeWidth={1.5} 
+                                            stroke="currentColor" 
+                                            className="size-6 text-uiDark inline"
                                             onClick={() => startEdit(idx)}
-                                            className="text-blue-600 hover:underline"
                                         >
-                                            Edit
-                                        </button>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                        </svg>
+
                                     )}
                                 </td>
                             )}
